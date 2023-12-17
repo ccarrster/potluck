887084b34ae3787076c881cefc7c59741abc7196
Chris <chris.carr@cobersolutions.com>
2023-12-17 15:38:53 -0500

    initial display

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,140 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <Potluck />
+    </>
+  )
+}
+
+function Potluck(){
+  let name = 'Frozen Cupcakes :)';
+  let date = 'Dec 18, 2023';
+  let time = '6:00 PM';
+  let invited = ['Daddy', 'Mommy', 'Ellott', 'Jasper', 'Sydney', 'Ben', 'Sophia', 'Freya'];
+  let needs = ['Icing - white', 'Sprinkles', 'Flour', 'Salt', 'Sugar', 'Eggs', 'Milk', 'Baking Soda', 'Chcolate Icing'];
+  let supplies = ['Oven', 'Oven Mitt', 'Cupcake Tin', 'Liners'];
+  let jobs = ['Baker', 'Cleaner', 'Decorator'];
+  let bringingSupplies = [[0, 0], [0, 2], [2, 1]];
+  let bringingJobs = [[0, 1], [0, 0], [2, 2]];
+  let bringingNeeds = [[2, 1], [4, 5], [4, 4], [4, 2], [5, 3], [5, 0], [1, 6], [1, 7]];
+  return (
+    <>
+      <div>
+        Event: {name}      
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        When: {date} {time}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        Who: <Invited people={invited}/>
       </div>
-    </main>
+      <div>
+        <Ingredients ingredients={needs} bringingNeeds={bringingNeeds}/>
+      </div>
+      <div>
+        <Jobs jobs={jobs} bringingJobs={bringingJobs}/>
+      </div>
+      <div>
+        <Supplies supplies={supplies} bringingSupplies={bringingSupplies}/>
+      </div>
+      <div>
+        <Bringing invited={invited} needs={needs} supplies={supplies} jobs={jobs} bs={bringingSupplies} bj={bringingJobs} bn={bringingNeeds}/>
+      </div>
+    </>
+  )
+}
+
+function Invited({people}){
+  let out = people.map((person, index) => {
+    return <div>{person}</div>
+  });
+  return (
+    <>
+    {out}
+    </>
+  )
+}
+
+function Ingredients({ingredients, bringingNeeds}){
+  let out = ingredients.map((ingredient, index) => {
+    let stillNeeded = true;
+    bringingNeeds.forEach((personNeeds, bnIndex) => {
+      if(personNeeds[1] == index){
+        stillNeeded = false;
+      }
+    });
+    return <div className={stillNeeded ? "ingredients" : "ingredients got"}>{ingredient}</div>
+  });
+  return (
+    <>
+    <div>Ingredients</div>
+    {out}
+    </>
+  )
+}
+
+function Jobs({jobs, bringingJobs}){
+  let stillNeeded = true;
+  let out = jobs.map((job, index) => {
+    bringingJobs.forEach((personJobs, bjIndex) => {
+      if(personJobs[1] == index){
+        stillNeeded = false;
+      }
+    });
+    return <div className={stillNeeded ? "ingredients" : "ingredients got"}>{job}</div>
+  });
+  return (
+    <>
+    <div>Jobs</div>
+    {out}
+    </>
+  )
+}
+
+function Supplies({supplies, bringingSupplies}){
+  let stillNeeded= true;
+  let out = supplies.map((supply, index) => {
+    bringingSupplies.forEach((personSupplies, bsIndex) => {
+      if(personSupplies[1] == index){
+        stillNeeded = false;
+      }
+    });
+    return <div className={stillNeeded ? "ingredients" : "ingredients got"}>{supply}</div>
+  });
+  return (
+    <>
+    <div>Supplies</div>
+    {out}
+    </>
+  )
+}
+
+function Bringing({invited, needs, supplies, jobs, bs, bj, bn}){
+  let people = [];
+  invited.forEach((person, personIndex) => {
+    let bringing = [];
+    bs.forEach((personSupplies, bsIndex) => {
+      if(personSupplies[0] == personIndex){
+        bringing.push(<div>{supplies[personSupplies[1]]}</div>);
+      }
+    });
+    bj.forEach((personJobs, bjIndex) => {
+      if(personJobs[0] == personIndex){
+        bringing.push(<div>{jobs[personJobs[1]]}</div>);
+      }
+    });
+    bn.forEach((personNeeds, bnIndex) => {
+      if(personNeeds[0] == personIndex){
+        bringing.push(<div>{needs[personNeeds[1]]}</div>);
+      }
+    });
+    people.push(<div>{person} {personIndex} {bringing}</div>);
+  });
+  return (
+    <>
+    <div>Bringing</div>
+    {people}
+    </>
   )
 }
