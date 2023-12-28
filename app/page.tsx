73d37767fe4a3c9fc8b06391eb9b4d7372dd5dc6
Chris <chris.carr@cobersolutions.com>
2023-12-28 09:30:50 -0500

    Add supplies and style

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
   return (
@@ -8,17 +10,51 @@ export default function Home() {
   )
 }
 
+let startSupplies = ['Oven', 'Oven Mitt', 'Cupcake Tin', 'Liners'];
+
 function Potluck(){
+  const [visible, setVisible] = useState(null);
+  const [supplies, setSupplies] = useState(startSupplies);
+
   let name = 'Frozen Cupcakes :)';
   let date = 'Dec 18, 2023';
   let time = '6:00 PM';
   let invited = ['Daddy', 'Mommy', 'Ellott', 'Jasper', 'Sydney', 'Ben', 'Sophia', 'Freya'];
   let needs = ['Icing - white', 'Sprinkles', 'Flour', 'Salt', 'Sugar', 'Eggs', 'Milk', 'Baking Soda', 'Chcolate Icing'];
-  let supplies = ['Oven', 'Oven Mitt', 'Cupcake Tin', 'Liners'];
   let jobs = ['Baker', 'Cleaner', 'Decorator'];
   let bringingSupplies = [[0, 0], [0, 2], [2, 1]];
   let bringingJobs = [[0, 1], [0, 0], [2, 2]];
   let bringingNeeds = [[2, 1], [4, 5], [4, 4], [4, 2], [5, 3], [5, 0], [1, 6], [1, 7]];
+
+  function onPeopleClick(){
+    if(visible == 'people'){
+      setVisible(null);
+    } else {
+      setVisible('people');
+    }
+  }
+  function onIngredientsClick(){
+    if(visible == 'ingredients'){
+      setVisible(null);
+    } else {
+      setVisible('ingredients');
+    }
+  }
+  function onJobsClick(){
+    if(visible == 'jobs'){
+      setVisible(null);
+    } else {
+      setVisible('jobs');
+    }
+  }
+  function onSuppliesClick(){
+    if(visible == 'supplies'){
+      setVisible(null);
+    } else {
+      setVisible('supplies');
+    }
+  }
+
   return (
     <>
       <div>
@@ -27,17 +63,17 @@ function Potluck(){
       <div>
         When: {date} {time}
       </div>
-      <div>
-        Who: <Invited people={invited}/>
+      <div className='border drop-shadow p-2 m-2 w-1/2' onClick={onPeopleClick}>
+        <Invited people={invited} visible={visible}/>
       </div>
-      <div>
-        <Ingredients ingredients={needs} bringingNeeds={bringingNeeds}/>
+      <div className='border drop-shadow p-2 m-2 w-1/2' onClick={onIngredientsClick}>
+        <Ingredients ingredients={needs} bringingNeeds={bringingNeeds} visible={visible}/>
       </div>
-      <div>
-        <Jobs jobs={jobs} bringingJobs={bringingJobs}/>
+      <div className='border drop-shadow p-2 m-2 w-1/2' onClick={onJobsClick}>
+        <Jobs jobs={jobs} bringingJobs={bringingJobs} visible={visible}/>
       </div>
-      <div>
-        <Supplies supplies={supplies} bringingSupplies={bringingSupplies}/>
+      <div className='border drop-shadow p-2 m-2 w-1/2' onClick={onSuppliesClick}>
+        <Supplies supplies={supplies} bringingSupplies={bringingSupplies} visible={visible} setSupplies={setSupplies}/>
       </div>
       <div>
         <Bringing invited={invited} needs={needs} supplies={supplies} jobs={jobs} bs={bringingSupplies} bj={bringingJobs} bn={bringingNeeds}/>
@@ -46,18 +82,25 @@ function Potluck(){
   )
 }
 
-function Invited({people}){
+function Invited({people, visible}){
   let out = people.map((person, index) => {
-    return <div>{person}</div>
+    return <li>{person}</li>
   });
+  let newClass = 'hidden';
+  if(visible == 'people'){
+    newClass = 'block';
+  }
   return (
     <>
+    <div>People</div>
+    <ul className={newClass}>
     {out}
+    </ul>
     </>
   )
 }
 
-function Ingredients({ingredients, bringingNeeds}){
+function Ingredients({ingredients, bringingNeeds, visible}){
   let out = ingredients.map((ingredient, index) => {
     let stillNeeded = true;
     bringingNeeds.forEach((personNeeds, bnIndex) => {
@@ -65,17 +108,23 @@ function Ingredients({ingredients, bringingNeeds}){
         stillNeeded = false;
       }
     });
-    return <div className={stillNeeded ? "ingredients" : "ingredients got"}>{ingredient}</div>
+    return <li className={stillNeeded ? "ingredients" : "ingredients got"}>{ingredient}</li>
   });
+  let newClass = 'hidden';
+  if(visible == 'ingredients'){
+    newClass = 'block';
+  }
   return (
     <>
     <div>Ingredients</div>
+    <ul className={newClass}>
     {out}
+    </ul>
     </>
   )
 }
 
-function Jobs({jobs, bringingJobs}){
+function Jobs({jobs, bringingJobs, visible}){
   let stillNeeded = true;
   let out = jobs.map((job, index) => {
     bringingJobs.forEach((personJobs, bjIndex) => {
@@ -83,30 +132,56 @@ function Jobs({jobs, bringingJobs}){
         stillNeeded = false;
       }
     });
-    return <div className={stillNeeded ? "ingredients" : "ingredients got"}>{job}</div>
+    return <li className={stillNeeded ? "ingredients" : "ingredients got"}>{job}</li>
   });
+  let newClass = 'hidden';
+  if(visible == 'jobs'){
+    newClass = 'block';
+  }
   return (
     <>
     <div>Jobs</div>
+    <ul className={newClass}>
     {out}
+    </ul>
     </>
   )
 }
 
-function Supplies({supplies, bringingSupplies}){
-  let stillNeeded= true;
+function Supplies({supplies, bringingSupplies, visible, setSupplies}){
+  const [newSupplies, setNewSupplies] = useState('');
+
+  
+  console.log(bringingSupplies);
   let out = supplies.map((supply, index) => {
+    let stillNeeded= true;
     bringingSupplies.forEach((personSupplies, bsIndex) => {
       if(personSupplies[1] == index){
         stillNeeded = false;
       }
     });
-    return <div className={stillNeeded ? "ingredients" : "ingredients got"}>{supply}</div>
+    return <li className={stillNeeded ? "ingredients" : "ingredients got"}>{supply}</li>
   });
+  let newClass = 'hidden';
+  if(visible == 'supplies'){
+    newClass = 'block';
+  }
+  function handleAddSupplies(){
+    let clone = supplies.slice(0);
+    clone.push(newSupplies);
+    setSupplies(clone);
+    setNewSupplies('');
+  }
+  function handleNewSupplies(e){
+    setNewSupplies(e.target.value);
+  }
   return (
     <>
     <div>Supplies</div>
+    <div><input value={newSupplies} onChange={handleNewSupplies}></input><button onClick={handleAddSupplies}>Add Supplies</button></div>
+    <ul className={newClass}>
     {out}
+    </ul>
     </>
   )
 }
@@ -117,25 +192,27 @@ function Bringing({invited, needs, supplies, jobs, bs, bj, bn}){
     let bringing = [];
     bs.forEach((personSupplies, bsIndex) => {
       if(personSupplies[0] == personIndex){
-        bringing.push(<div>{supplies[personSupplies[1]]}</div>);
+        bringing.push(<li>{supplies[personSupplies[1]]}</li>);
       }
     });
     bj.forEach((personJobs, bjIndex) => {
       if(personJobs[0] == personIndex){
-        bringing.push(<div>{jobs[personJobs[1]]}</div>);
+        bringing.push(<li>{jobs[personJobs[1]]}</li>);
       }
     });
     bn.forEach((personNeeds, bnIndex) => {
       if(personNeeds[0] == personIndex){
-        bringing.push(<div>{needs[personNeeds[1]]}</div>);
+        bringing.push(<li>{needs[personNeeds[1]]}</li>);
       }
     });
-    people.push(<div>{person} {personIndex} {bringing}</div>);
+    people.push(<div className='border'><ul>{person} {personIndex} {bringing}</ul></div>);
   });
   return (
     <>
     <div>Bringing</div>
+    <div className='flex'>
     {people}
+    </div>
     </>
   )
 }
